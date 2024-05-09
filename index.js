0d7cb3c73b1d7b2169341b559e62d956b14bf7b8
hooriya <parih3080@gmail.com>
2024-05-10 02:43:01 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// QUESTION 100:
+let mathObject = Math.sqrt(144);
+console.log(mathObject);
+// QUESTION 101:
+function randomInteger() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+console.log(randomInteger());
+// QUESTION 102:
+function calculateLog() {
+    return Math.abs(-5 - 5);
+}
+let differnce = calculateLog();
+console.log(differnce);
+export {};
